cc44a84b6ca2c8cb0bd3871ea775c8ea76208551
Vinay Rathore <vinayrathore87@gmail.com>
2024-11-15 22:15:24 +0530

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+// import { Routes } from '@angular/router';
+
+// export const routes: Routes = [];
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'customer',
+    loadChildren: () => import('./customers/customers.module')
+      .then(mod => mod.CustomersModule)
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./orders/orders.module')
+      .then(mod => mod.OrdersModule)
+  },
+
+];
